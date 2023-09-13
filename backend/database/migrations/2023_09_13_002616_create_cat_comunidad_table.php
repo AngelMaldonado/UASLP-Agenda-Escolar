daf33604924ea148cc7f472964d3d813ecb5a319
AngelMaldonado <amaldonadojuarez@gmail.com>
2023-09-12 19:49:09 -0600

    chore: migraciones de tablas

--- backend/database/migrations/2023_09_13_002616_create_cat_comunidad_table.php
+++ backend/database/migrations/2023_09_13_002616_create_cat_comunidad_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cat_comunidad', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('descripcion', 250);
         });
     }
 
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cat_comunidad');
     }
 };
