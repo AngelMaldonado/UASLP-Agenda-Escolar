cd970eca6500c8cc2dd66bc138b358448cf090de
Jose Alfredo Gonzalez Cruz <98406834+AlfredoGlz2221@users.noreply.github.com>
2023-09-17 10:18:22 -0600

    Usuarios Componentes
    back y front

---
+++ frontend/src/components/FormularioAdministrador/InicioAdministrador.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Inicio</title>
+</head>
+<body>
+    <h2>Bienvenido</h2>
+    <p>Has iniciado sesión con éxito.</p>
+    <a href="cerrar_sesion.php">Cerrar Sesión</a>
+</body>
+</html>
---
+++ frontend/src/components/FormularioUsuario/InicioNuevoUsuario.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Inicio ALUMNO</title>
+</head>
+<body>
+    <h2>Bienvenido ALUMNO</h2>
+    <p>Has iniciado sesión con éxito.</p>
+    <a href="cerrar_sesion.php">Cerrar Sesión</a>
+</body>
+</html>
