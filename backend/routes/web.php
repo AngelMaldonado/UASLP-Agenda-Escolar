b4121a851dbad5635fc053ed54bb9eb55f8f0e8d
AngelMaldonado <amaldonadojuarez@gmail.com>
2023-09-21 10:57:52 -0600

    chore: avance de manejo de rutas para consultas de Usuario

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,22 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminUserController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\UsuarioController;
+
 //Ruta para el CRUD de usuarios
-Route::middleware(['auth'])->group(function () {
-    Route::resource('admin/users', AdminUserController::class);
+Route::middleware(['auth'])->get('/user', function (Request $request) {
+    return $request->user();
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(UsuarioController::class)->group(function (){
+    Route::get('/usuarios', 'index');
+    Route::post('/usuarios', 'store');
 });
