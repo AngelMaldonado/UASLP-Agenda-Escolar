31b16186af1d3dd3e6a453962e3b2d1e96432fd2
ManuelRS <a205813@alumnos.uaslp.mx>
2023-10-19 16:59:44 -0600

    Cambios de en controladores para los eventos en calendario

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\EventController;
 
 //Ruta para el CRUD de usuarios
 Route::middleware(['auth'])->get('/user', function (Request $request) {
     return $request->user();
 });
+//Ruta para mostrar eventos en FullCalendar
+Route::get('/eventos/{fecha}', [EventController::class, 'showEventsByDay']);
+
 
 Route::controller(UsuarioController::class)->group(function (){
     Route::get('/usuarios', 'index');
