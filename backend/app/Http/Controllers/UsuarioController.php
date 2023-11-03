29d11398350813b73574c888aa0276ad47c3e11e
AngelMaldonado <amaldonadojuarez@gmail.com>
2023-11-03 13:24:54 -0600

    fix: importaciones \Http y \Models repetidas

--- backend/app/Http/Controllers/UsuarioController.php
+++ backend/app/Http/Controllers/UsuarioController.php
@@ -46,9 +46,6 @@ class UsuarioController extends Controller
         return response()->json($usuario);
     }
 
-    use Illuminate\Http\Request;
-    use App\Models\Usuario;
-    
     public function update(Request $request)
     {
         // Definición de reglas de validación para los campos del objeto JSON
@@ -58,7 +55,7 @@ class UsuarioController extends Controller
             'email' => 'required|string|email|unique:usuario,email,' . $request->input('id'), // El email es obligatorio, debe ser una dirección de correo electrónico válida y único en la tabla de usuarios, excepto para el usuario con el mismo ID.
             'permisos' => 'required|json', // Asegúrate de que los datos del campo "permisos" se manejen adecuadamente como JSON.
         ];
-    
+
         // Definición de mensajes de error personalizados
         $messages = [
             'required' => 'El campo :attribute es obligatorio.',
@@ -68,15 +65,15 @@ class UsuarioController extends Controller
             'unique' => 'El campo :attribute ya está en uso.',
             'json' => 'El campo :attribute debe ser un JSON válido.',
         ];
-    
+
         // Validación de la solicitud usando las reglas definidas
         $validator = Validator::make($request->all(), $rules, $messages);
-    
+
         // Comprobar si la validación falló
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 400); // Respuesta de error con mensajes de validación y código de estado 400 (Bad Request).
         }
-    
+
         // Actualizar el usuario en la base de datos
         $usuario = Usuario::findOrFail($request->input('id'));
         $usuario->nombre = $request->input('nombre');
@@ -84,11 +81,11 @@ class UsuarioController extends Controller
         $usuario->email = $request->input('email');
         $usuario->permisos = $request->input('permisos'); // Asegúrate de que los datos del campo "permisos" se manejen adecuadamente como JSON.
         $usuario->save();
-    
+
         // Respuesta exitosa con el usuario actualizado en formato JSON y código de estado 200 (OK).
         return response()->json($usuario, 200);
     }
-    
+
 
     public function destroy($id): \Illuminate\Http\JsonResponse
     {
