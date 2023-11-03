610ed44b650056a1d14d080ff4e2c96cf7db0259
AngelMaldonado <amaldonadojuarez@gmail.com>
2023-11-03 14:00:40 -0600

    fix: cambio de "puesto" por "tipo" en update()

--- backend/app/Http/Controllers/UsuarioController.php
+++ backend/app/Http/Controllers/UsuarioController.php
@@ -48,38 +48,28 @@ class UsuarioController extends Controller
 
     public function update(Request $request)
     {
-        // Definición de reglas de validación para los campos del objeto JSON
-        $rules = [
-            'nombre' => 'required|string|max:50',  // El nombre es obligatorio y no debe exceder 50 caracteres.
-            'puesto' => 'required|string|max:30',  // El puesto es obligatorio y no debe exceder 30 caracteres.
-            'email' => 'required|string|email|unique:usuario,email,' . $request->input('id'), // El email es obligatorio, debe ser una dirección de correo electrónico válida y único en la tabla de usuarios, excepto para el usuario con el mismo ID.
-            'permisos' => 'required|json', // Asegúrate de que los datos del campo "permisos" se manejen adecuadamente como JSON.
-        ];
+        // Validar los datos recibidos en la solicitud
+        $request->validate([
+            'id' => 'required|exists:usuario', // Asegurarse de que el usuario exista
+            'nombre' => 'required|string|max:50',
+            'tipo' => 'required|string|max:30',
+            'email' => 'required|string|email|unique:usuario,email,' . $request->input('id'),
+            'permisos' => 'required|array', // Asegurarse de que los datos de permisos se manejen adecuadamente como JSON.
+        ]);
 
-        // Definición de mensajes de error personalizados
-        $messages = [
-            'required' => 'El campo :attribute es obligatorio.',
-            'string' => 'El campo :attribute debe ser una cadena de caracteres.',
-            'max' => 'El campo :attribute no debe exceder los :max caracteres.',
-            'email' => 'El campo :attribute debe ser una dirección de correo electrónico válida.',
-            'unique' => 'El campo :attribute ya está en uso.',
-            'json' => 'El campo :attribute debe ser un JSON válido.',
-        ];
+        // Obtener el ID del usuario a actualizar
+        $userId = $request->input('id');
 
-        // Validación de la solicitud usando las reglas definidas
-        $validator = Validator::make($request->all(), $rules, $messages);
+        // Buscar el usuario en la base de datos
+        $usuario = Usuario::findOrFail($userId);
 
-        // Comprobar si la validación falló
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 400); // Respuesta de error con mensajes de validación y código de estado 400 (Bad Request).
-        }
-
-        // Actualizar el usuario en la base de datos
-        $usuario = Usuario::findOrFail($request->input('id'));
+        // Actualizar los datos del usuario con los valores recibidos en la solicitud
         $usuario->nombre = $request->input('nombre');
-        $usuario->puesto = $request->input('puesto');
+        $usuario->tipo = $request->input('tipo');
         $usuario->email = $request->input('email');
-        $usuario->permisos = $request->input('permisos'); // Asegúrate de que los datos del campo "permisos" se manejen adecuadamente como JSON.
+        $usuario->permisos = $request->input('permisos'); // Asegurarse de que los datos de permisos se manejen adecuadamente como JSON.
+
+        // Guardar los cambios en la base de datos
         $usuario->save();
 
         // Respuesta exitosa con el usuario actualizado en formato JSON y código de estado 200 (OK).
