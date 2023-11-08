75aa46c5d7baf6b77e1cc25c03202bd9970270e2
ManuelRS <a205813@alumnos.uaslp.mx>
2023-11-08 09:33:48 -0600

    Modificación a las migraciones en español

--- backend/app/Http/Controllers/EventController.php
+++ backend/app/Http/Controllers/EventController.php
@@ -10,89 +10,77 @@ use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller
 {
-    // Función para almacenar un nuevo evento
     public function store(Request $request)
     {
-        // Validar los datos de la solicitud utilizando las reglas de validación
         $request->validate([
-            'title' => 'required|max:255',  // El título es obligatorio y no debe exceder los 255 caracteres.
-            'start_date' => 'required|date', // La fecha de inicio es obligatoria y debe ser una fecha válida.
-            'end_date' => [ // La fecha de fin es obligatoria, debe ser una fecha válida y debe ser posterior o igual a la fecha de inicio.
-                'required',
-                'date',
-                'after_or_equal:start_date',
-            ],
+            'nombre' => 'required|max:100', // El campo 'nombre' es obligatorio y no debe exceder los 100 caracteres.
+            'cat_evento_id' => 'required|exists:cat_evento,id', // El campo 'cat_evento_id' es obligatorio y debe existir en la tabla 'cat_evento'.
+            'usuario_id' => 'required|exists:usuario,id', // El campo 'usuario_id' es obligatorio y debe existir en la tabla 'usuario'.
+            'nombre' => 'required|max:100', // El campo 'nombre' es obligatorio y no debe exceder los 100 caracteres.
+            'fecha_inicio' => 'required|date', // El campo 'fecha_inicio' es obligatorio y debe ser una fecha válida.
+            'fecha_fin' => 'required|date|after_or_equal:fecha_inicio', // El campo 'fecha_fin' es obligatorio, debe ser una fecha válida y debe ser posterior o igual a 'fecha_inicio'.
+            'hipervinculos' => 'json', // Asegúrate de que los datos se manejen adecuadamente como JSON.
+            'imagen' => 'required|string|max:200', // El campo 'imagen' es obligatorio, debe ser una cadena de caracteres y no debe exceder los 200 caracteres.
+            'descripcion' => 'required|string|max:250', // El campo 'descripcion' es obligatorio, debe ser una cadena de caracteres y no debe exceder los 250 caracteres.
+            'tipo' => 'required|string|max:10', // El campo 'tipo' es obligatorio, debe ser una cadena de caracteres y no debe exceder los 10 caracteres.
         ]);
 
-        // Crear un nuevo evento en la base de datos utilizando los datos de la solicitud.
         $event = Event::create($request->all());
 
-        // Retornar el evento creado en formato JSON como respuesta.
         return response()->json($event);
     }
 
-    // Función para actualizar un evento existente
     public function update(Request $request, $id)
     {
-        // Validar los datos de la solicitud utilizando las mismas reglas de validación.
         $request->validate([
-            'title' => 'required|max:255',
-            'start_date' => 'required|date',
-            'end_date' => [
-                'required',
-                'date',
-                'after_or_equal:start_date',
-            ],
+            'nombre' => 'required|max:100', // El campo 'nombre' es obligatorio y no debe exceder los 100 caracteres.
+            'cat_evento_id' => 'required|exists:cat_evento,id', // El campo 'cat_evento_id' es obligatorio y debe existir en la tabla 'cat_evento'.
+            'usuario_id' => 'required|exists:usuario,id', // El campo 'usuario_id' es obligatorio y debe existir en la tabla 'usuario'.
+            'nombre' => 'required|max:100', // El campo 'nombre' es obligatorio y no debe exceder los 100 caracteres.
+            'fecha_inicio' => 'required|date', // El campo 'fecha_inicio' es obligatorio y debe ser una fecha válida.
+            'fecha_fin' => 'required|date|after_or_equal:fecha_inicio', // El campo 'fecha_fin' es obligatorio, debe ser una fecha válida y debe ser posterior o igual a 'fecha_inicio'.
+            'hipervinculos' => 'json', // Asegúrate de que los datos se manejen adecuadamente como JSON.
+            'imagen' => 'required|string|max:200', // El campo 'imagen' es obligatorio, debe ser una cadena de caracteres y no debe exceder los 200 caracteres.
+            'descripcion' => 'required|string|max:250', // El campo 'descripcion' es obligatorio, debe ser una cadena de caracteres y no debe exceder los 250 caracteres.
+            'tipo' => 'required|string|max:10', // El campo 'tipo' es obligatorio, debe ser una cadena de caracteres y no debe exceder los 10 caracteres.
         ]);
 
-        // Buscar el evento en la base de datos por su identificador.
         $event = Event::find($id);
 
-        // Si el evento no se encuentra, devolver una respuesta de error con un código de estado 404.
         if (!$event) {
             return response()->json(['message' => 'Evento no encontrado'], 404);
         }
 
-        // Actualizar el evento con los datos de la solicitud.
         $event->update($request->all());
 
-        // Retornar el evento actualizado en formato JSON como respuesta.
         return response()->json($event);
-    }
-
-    //Tipo DELETE
-    public function destroy(Request $request, $id)
-    {
-        // Validar que el evento existe en la base de datos
-        $event = Event::find($id);
-
-        if (!$event) {
-            return response()->json(['message' => 'Evento no encontrado'], 404); // Respuesta de error con código 404 (No encontrado).
-        }
-
-        // Eliminar el evento de la base de datos
-        $event->delete();
-
-        // Respuesta exitosa con un mensaje en formato JSON y código de estado 200 (OK).
-        return response()->json(['message' => 'Evento eliminado con éxito'], 200);
     }
 
     public function EventosCalendario(Request $request)
     {
         $mes = $request->input('mes');
 
-        // Validar si el valor del parámetro "mes" es un número del 1 al 12 o "all"
         if ($mes === 'all') {
-            // Consulta para obtener eventos de todos los meses ordenados por fecha
             $eventos = Event::orderBy('fecha_inicio', 'asc')->get();
         } elseif (is_numeric($mes) && $mes >= 1 && $mes <= 12) {
-            // Consulta para obtener eventos del mes especificado
             $eventos = Event::whereMonth('fecha_inicio', $mes)->orderBy('fecha_inicio', 'asc')->get();
         } else {
-            // Valor no válido para el parámetro "mes"
             return response()->json(['message' => 'Valor de mes no válido'], 400);
         }
 
-        return response()->json($eventos);}
+        return response()->json($eventos);
+    }
 
+    public function destroy(Request $request, $id)
+    {
+        $event = Event::find($id);
+
+        if (!$event) {
+            return response()->json(['message' => 'Evento no encontrado'], 404);
+        }
+
+        $event->delete();
+
+        return response()->json(['message' => 'Evento eliminado con éxito'], 200);
+    }
 }
