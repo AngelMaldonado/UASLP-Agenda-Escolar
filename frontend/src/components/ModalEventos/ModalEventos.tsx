df08f1e697fb2824d5b27db024f40600167f9346
Jose Alfredo Gonzalez Cruz <98406834+AlfredoGlz2221@users.noreply.github.com>
2023-11-17 08:52:31 -0600

    chore: uso de constantes tarjeta simbolos, login.

--- frontend/src/components/ModalEventos/ModalEventos.tsx
+++ frontend/src/components/ModalEventos/ModalEventos.tsx
@@ -1,28 +1,26 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import TarjetaLarga from '../CardAgenda/CardAgenda.tsx';
-import Evento from '../../models/Evento';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import TarjetaLarga from "../CardAgenda/CardAgenda.tsx";
+import Evento from "../../models/Evento";
 
 function ModalEventos() {
-    let evento = new Evento;
-    const [show, setShow] = useState(false);
+  let evento = new Evento();
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div
       className="modal show"
-      style={{ display: 'block', position: 'initial' }}
+      style={{ display: "block", position: "initial" }}
     >
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal Eventos</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>
-          Hola
-        </Modal.Body>
+        <Modal.Body>Hola</Modal.Body>
 
         <Modal.Footer>
           <Button variant="primary">Agregar Evento</Button>
