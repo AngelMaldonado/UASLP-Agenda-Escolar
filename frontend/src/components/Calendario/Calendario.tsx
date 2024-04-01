aff4fadb1342e68fbfcb0cb89101f05ee6047f3b
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-03-31 23:33:54 -0600

    advance: mejoras en renderización del calendario y límite de eventos en el calendario

--- frontend/src/components/Calendario/Calendario.tsx
+++ frontend/src/components/Calendario/Calendario.tsx
@@ -46,11 +46,12 @@ function Calendario(props: CalendarioProps) {
                         ({...prevState, mes: meses.get(titulo[0].toLowerCase())!})
                       )
                     }}
-                    titleFormat={{year: "numeric", month: "short"}}
+                    dayMaxEventRows
+                    titleFormat={{year: "numeric", month: "long"}}
                     dayHeaderFormat={{weekday: "long"}}
                     events={events}
                     eventContent={cardEvento}
-                    eventClassNames={"evento-calendario my-1"}
+                    eventClassNames={"evento-calendario"}
                     eventBackgroundColor={"transparent"}
       />
       <CardsContenedor admin={props.admin}/>
@@ -60,13 +61,19 @@ function Calendario(props: CalendarioProps) {
   function cardEvento(arg: any) {
     const evento = arg.event.extendedProps
     return (
-      <div className="px-2 py-1 w-100 text-center text-dark rounded-4"
-           style={{backgroundImage: `url(${Configuraciones.publicURL + evento.simbolo})`}}
-      >
-        <p className="px-2 m-0 bg-light bg-opacity-75 rounded-4 titulo-evento-calendario w-100"
+      <div className="px-1 py-1 w-100 text-center text-dark"
            style={{
-             width: evento.fecha_fin > evento.fecha_inicio ? "fit-content" : "100%"
+             backgroundImage: `url(${Configuraciones.publicURL + evento.simbolo})`,
+             fontSize: "0.8rem"
            }}
+      >
+        <p
+          className={
+            `px-1 m-0 rounded-4 titulo-evento-calendario ${arg.isStart ? "bg-light bg-opacity-75" : "opacity-0"}`
+          }
+          style={{
+            width: evento.fecha_fin > evento.fecha_inicio ? "fit-content" : "100%"
+          }}
         >
           {arg.event.extendedProps.nombre}
         </p>
