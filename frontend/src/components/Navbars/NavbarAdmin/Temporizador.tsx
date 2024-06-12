d9af054ba25515b0bc38ef3aebfe393f4a761860
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-06-11 18:54:22 -0600

    fix: redirección a login cuando el tiempo termina

--- frontend/src/components/Navbars/NavbarAdmin/Temporizador.tsx
+++ frontend/src/components/Navbars/NavbarAdmin/Temporizador.tsx
@@ -5,6 +5,7 @@ import {Stack} from "react-bootstrap";
 import useTemporizador from "../../../hooks/HookTemporizador.ts";
 import {useLogout} from "../../../hooks/HookAutenticacion.ts";
 import Modal from "../../Modales/Modal";
+import {Navigate} from "react-router-dom";
 
 export function Temporizador() {
   const {sesion} = useObtenSesion()
@@ -12,34 +13,37 @@ export function Temporizador() {
   const {logout} = useLogout()
   const [horas, minutos, segundos] = useTemporizador(new Date(sesion?.expiracion!))
 
-  return (
-    <Stack gap={2} className="Temporizador align-items-center">
-      <p className="m-0">
-        Tiempo
-        restante: {horas >= 10 ? horas : `0${horas}`}:{minutos >= 10 ? minutos : `0${minutos}`}:{segundos >= 10 ? segundos : `0${segundos}`}
-      </p>
-      <Boton variant={TemaComponente.Secundario}
-             disabled={!(horas == 0 && minutos < 5)}
-             etiqueta="Extender sesión"
-             onClick={extiendeSesion}
-      />
-      <Boton variant={TemaComponente.Danger}
-             etiqueta="Cerrar sesión"
-             onClick={logout}
-      />
-      {horas == 0 && minutos == 5 ?
-        <Modal mostrar sinFondo
-               contenido={<p className="text-center">Su sesión está apunto de caducar, ¿desea extender su sesión?</p>}
-               cancelar
-               botones={[
-                 <Boton key={"boton-extender"}
-                        variant={TemaComponente.PrimarioInverso}
-                        etiqueta="Extender (+20 min)"
-                        onClick={extiendeSesion}
-                 />,
-               ]}
+  if (horas < 0 || minutos < 0 || segundos < 0)
+    return (<Navigate to='/login'/>)
+  else
+    return (
+      <Stack gap={2} className="Temporizador align-items-center">
+        <p className="m-0">
+          Tiempo
+          restante: {horas >= 10 ? horas : `0${horas}`}:{minutos >= 10 ? minutos : `0${minutos}`}:{segundos >= 10 ? segundos : `0${segundos}`}
+        </p>
+        <Boton variant={TemaComponente.Secundario}
+               disabled={!(horas == 0 && minutos < 5)}
+               etiqueta="Extender sesión"
+               onClick={extiendeSesion}
         />
-        : null}
-    </Stack>
-  );
+        <Boton variant={TemaComponente.Danger}
+               etiqueta="Cerrar sesión"
+               onClick={logout}
+        />
+        {horas == 0 && minutos == 5 ?
+          <Modal mostrar sinFondo
+                 contenido={<p className="text-center">Su sesión está apunto de caducar, ¿desea extender su sesión?</p>}
+                 cancelar
+                 botones={[
+                   <Boton key={"boton-extender"}
+                          variant={TemaComponente.PrimarioInverso}
+                          etiqueta="Extender (+20 min)"
+                          onClick={extiendeSesion}
+                   />,
+                 ]}
+          />
+          : null}
+      </Stack>
+    );
 }
