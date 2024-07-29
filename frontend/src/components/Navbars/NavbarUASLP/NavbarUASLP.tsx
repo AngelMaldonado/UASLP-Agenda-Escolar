56b0e84b10fdea294a621f3c07efa8c98fb09bb4
AngelMaldonado <amaldonadojuarez@gmail.com>
2024-07-29 10:26:21 -0600

    #10-Solucionando problema de render de NavbarUASLP en Firefox

--- frontend/src/components/Navbars/NavbarUASLP/NavbarUASLP.tsx
+++ frontend/src/components/Navbars/NavbarUASLP/NavbarUASLP.tsx
@@ -1,4 +1,4 @@
-import {Image} from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo_ingenieria from "../../../assets/logo_ingenieria.svg"
@@ -7,13 +7,13 @@ import logo_uaslp from "../../../assets/logo_uaslp.svg"
 export default function NavbarUASLP() {
   return (
     <Nav className="NavbarUASLP navbar-expand py-2">
-      <Container className="d-flex gap-3 p-4">
+      <Container className="d-flex justify-content-start gap-3 p-4">
         <a href="https://www.uaslp.mx/">
-          <Image fluid className="h-100" src={logo_uaslp} alt="Logo uaslp"/>
+          <Image fluid className="h-100" src={logo_uaslp} width={174} alt="Logo uaslp" />
         </a>
         <div className="vr"></div>
         <a href="https://www.ingenieria.uaslp.mx/">
-          <Image fluid className="h-100" src={logo_ingenieria} alt="Logo ingenieria"/>
+          <Image fluid className="h-100" src={logo_ingenieria} width={153} alt="Logo ingenieria" />
         </a>
       </Container>
     </Nav>
